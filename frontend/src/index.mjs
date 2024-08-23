80da423c5893767f4d56ecfe2804b99a3081ed7b
Marco Facecchia <facecchiamarco@icloud.com>
2024-08-23 14:34:14 +0100

    Add initial ExpressJS frontend app setup

---
+++ frontend/src/index.mjs
@@ -0,0 +1,23 @@
+import express from 'express';
+import 'dotenv/config';
+
+
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+// Remove the line below if you want to show the 'x-powered-by' header in the response (NOT recommended)
+app.set('x-powered-by', false);
+
+
+// 404 status code error handling
+app.use((req, res) => {
+    // TODO: Render 404 page instead of JSON response
+    return res.status(404).json({
+        status: 404,
+        message: `Route ${req.baseUrl + req.path} not found.`
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Listening.");
+});
