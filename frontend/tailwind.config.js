bde5507fd1ec77d9331931df343babb891bc7885
Marco Facecchia <facecchiamarco@icloud.com>
2024-08-24 20:31:52 +0100

    Update tailwind config source files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'src/views/**/*.{ejs,html}',
+    'views/**/*.{ejs,html}',
     'src/*.js',
     'public/script/*.js'
   ],
