852e03211390a09d5f99d16f71642d1ddaba5e62
Marco Facecchia <facecchiamarco@icloud.com>
2024-09-19 00:07:37 +0200

    Backend: add email verification mysql query named export function

---
+++ backend/db/queries/mysql/emailVerification.mysql.query.js
@@ -0,0 +1,27 @@
+import { PrismaClientInitializationError, PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/library';
+import prisma from '../../../db/prismaClient.db.js';
+import { DatabaseConnectionError, DatabaseQueryError } from '../../../src/errors/custom.errors.js';
+import { logError } from '../../../src/errors/errorHandler.errors.js';
+
+
+export async function verifyEmail(userID){
+    try{
+        await prisma.credential.update({
+            data: {
+                verified: true
+            },
+            where: {
+                userID: +userID
+            }
+        });
+        return true;
+    }catch(err){
+        logError(err);
+        if(err instanceof PrismaClientInitializationError) throw new DatabaseConnectionError('Failed connection to database.');
+        else if(err instanceof PrismaClientKnownRequestError){
+            if(err.code === 'P2004') throw new DatabaseQueryError('Could not verify user email. Invalid data.');
+        }
+        else if(err instanceof PrismaClientValidationError) throw new DatabaseQueryError('Could not verify user email. Invalid or missing required data.');
+        throw new DatabaseQueryError('Unexpected error while verifying user email. Please try again later.');
+    }
+}
