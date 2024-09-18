04a2f15059d1eed2a1003f6f9bf1bc34f0d412c1
Marco Facecchia <facecchiamarco@icloud.com>
2024-09-18 14:46:02 +0200

    Backend: add user update PUT endpoint

--- backend/src/routes/user.routing.js
+++ backend/src/routes/user.routing.js
@@ -1,7 +1,12 @@
 import isLoggedIn from "../middlewares/isLoggedIn.middleware.js";
+import isCsrfTokenValid from "../middlewares/isCsrfTokenValid.middleware.js";
 import { handleError } from "../errors/errorHandler.errors.js";
+import { clearAllCookies } from "../utils/cookies.utility.js";
+import { updateUser } from "../../db/queries/mysql/user.mysql.query.js";
+import { hashPassword } from "../security/hashing.security.js";
 
 
+// TODO: Add validation & rate limit mws
 export default function userManagement(app){
     app.route('/api/user')
         .get(isLoggedIn({ strict: true, sendResponseOnValidToken: false, returnLastUserValues: true }), async (req, res) => {
@@ -18,5 +23,21 @@ export default function userManagement(app){
                 return await handleError(req, res, err);
             }
         })
-        
+        .put(isLoggedIn({ strict: true, sendResponseOnValidToken: false, returnLastUserValues: true }), isCsrfTokenValid(), async (req, res) => {
+            try{
+                const { firstName, lastName, email, password } = req.body;
+                const { userID } = req.lastUserValues;
+                let hashedPassword;
+                if(password) hashedPassword = await hashPassword(password);
+                const userData = await updateUser(userID, firstName, lastName, email, hashedPassword, { getFullInfo: true });
+                if(password || email) await clearAllCookies(req, res);
+                return res.status(200).json({
+                    status: 200,
+                    message: "User data successfully updated",
+                    verified: userData.credential[0].verified
+                });
+            }catch(err){
+                return await handleError(req, res, err);
+            }
+        })
 }
