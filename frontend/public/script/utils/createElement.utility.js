27c646bc639e9ca8a6d1a4c69365a25bd64e23b5
Marco Facecchia <facecchiamarco@icloud.com>
2024-09-19 11:38:19 +0200

    Backend: add 'HTMLElement' creator utility script

---
+++ frontend/public/script/utils/createElement.utility.js
@@ -0,0 +1,20 @@
+function createElement(elementName, attributes = {}, classes = []){
+    /*
+        * Creates an element with the given `elementName` and additional `attributes` and `classes` names
+        * The `attributes` parameter should be an Object with the attribute name as key and the relative value as Object value
+        * The `classes` parameter should be an Array with all the classes names as values
+        * * NOTE: The `attributes` parameter must be an `Object` and `classes` MUST be an `Array`
+        * Returns an instance of the chosen `elementName` element
+    */
+    const element = document.createElement(String(elementName));
+    if(isObject(attributes)){
+        // Applying all attributes to the new element
+        for(const [name, value] in Object.entries(attributes)){
+            element.setAttribute(name, value);
+        }
+    }
+    if(Array.isArray(classes)){
+        element.classList.add(...classes);
+    }
+    return element;
+}
