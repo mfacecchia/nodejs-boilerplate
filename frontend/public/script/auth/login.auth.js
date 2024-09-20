ff4e5b8ee484868ffc691dcf188fe893c9735351
Marco Facecchia <facecchiamarco@icloud.com>
2024-09-20 16:17:35 +0200

    Frontend: add user credentials login script

---
+++ frontend/public/script/auth/login.auth.js
@@ -0,0 +1,36 @@
+const loginForm = document.querySelector('#loginForm');
+
+loginForm.onsubmit = async (e) => {
+    e.preventDefault();
+    clearFormErrorMessages(loginForm, false);
+    let userData = formDataToObject(new FormData(loginForm));
+    try{
+        await validateLogin(userData);
+        // Overwriting the Object with the sanitized data version
+    }catch(err){
+        showFormValidationErrors(loginForm, err);
+        return;
+    }
+    try{
+        const fetchResult = await fetchFromServer('/user/login/credentials', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(userData),
+            credentials: "include"
+        });
+        const { jsonRes } = fetchResult;
+        // Backend validation not passed
+        if(jsonRes.validationErrors){
+            showFormValidationErrors(loginForm, jsonRes.validationErrors);
+            return;
+        }
+        if(!fetchResult.successful) throw new Error(jsonRes.message);
+        // Redirecting to homepage after successful authentication
+        // window.location.href = '/';
+    }catch(err){
+        displayMessage(err.message || 'Could not fetch data. Please try again later.', 'error');
+    }
+}
