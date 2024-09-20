e6aa1a0aeacdbda618c84f0e2d95daafba2d62e5
Marco Facecchia <facecchiamarco@icloud.com>
2024-09-20 16:17:50 +0200

    Frontend: add user credentials signup script

---
+++ frontend/public/script/auth/signup.auth.js
@@ -0,0 +1,38 @@
+const signupForm = document.querySelector('#signupForm');
+
+signupForm.onsubmit = async (e) => {
+    e.preventDefault();
+    clearFormErrorMessages(signupForm, false);
+    let newUserData = formDataToObject(new FormData(signupForm));
+    try{
+        await validateSignup(newUserData);
+    }catch(err){
+        showFormValidationErrors(signupForm, err);
+        return;
+    }
+    // displayMessage('Signing you up. Please wait.', 'info');
+    try{
+        const fetchResult = await fetchFromServer('/user/signup/credentials', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(newUserData)
+        });
+        const { jsonRes } = fetchResult;
+        // Backend validation not passed
+        if(jsonRes.validationErrors){
+            showFormValidationErrors(signupForm, jsonRes.validationErrors);
+            return;
+        }
+        if(!fetchResult.successful) throw new Error(jsonRes.message);
+        displayMessage(jsonRes.message, 'success');
+        // Redirecting to login page in case of successful signup
+        setTimeout(() => {
+            window.location.pathname = '/login';
+        }, 5000);
+    }catch(err){
+        displayMessage(err.message, 'error');
+    }
+}
